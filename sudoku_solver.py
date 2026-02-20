f722a1062e0eb9325836eeb9f0b0f76b4f3967ec
Jakub Nowicki <148172103+Jakub-Nowicki@users.noreply.github.com>
2026-02-20 13:42:13 -0600

    changes

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,115 +1,99 @@
-from flask import Flask, render_template, redirect, url_for, request
-
-def display_board(bo): #function to display board on the screen
-    for i in range(len(bo)):
-
-        if i % 3 == 0 and i != 0:
-            print('- - - - - - - - - - - -')
-
-        for j in range(len(bo[0])):
-
-            if j % 3 == 0 and j !=0: 
-                print(' | ', end='')
-
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(bo[i][j], end=' ')
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+import copy
 
 
 def solve(bo):
-
-    find = find_empty(bo) 
-
-    if not find: # we check if it is the end of the sudoku, and if there is something more to solve
+    find = find_empty(bo)
+    if not find:
         return True
-    else:
-        row, col = find
-
-    for i in range(1,10):
-    
+    row, col = find
+    for i in range(1, 10):
         if valid(bo, i, (row, col)):
             bo[row][col] = i
-            display_board(bo)
             if solve(bo):
-                return True  # true if the number might be right
-            
-            bo[row][col] = 0 # if the number is not right we gonna change it to the 0 and try with other previous one
+                return True
+            bo[row][col] = 0
+    return False
 
-    return False # we gonna return false if any of the numbers is correct in this currect positon so we have to backtrack
 
-        
-
-def find_empty(bo): # finding where the 0 is 
-    for i in  range(len(bo)):
+def find_empty(bo):
+    for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return (i, j) # we are returning row and column
+                return (i, j)
     return None
-            
 
-def valid(bo, num, pos): #checking if the number is valid 
 
-    for i in range(len(bo[0])): #checking row
+def valid(bo, num, pos):
+    for i in range(len(bo[0])):
         if bo[pos[0]][i] == num and pos[1] != i:
             return False
-        
-    for i in range(len(bo)): #checking col
+    for i in range(len(bo)):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
-        
-    row = pos[0] // 3 # if it is 2nd box the row will be 1 and so on
+    row = pos[0] // 3
     column = pos[1] // 3
-
-    #checking inside the box
-    for i in range(row*3, row*3 + 3): #iterating between rows we start at the beginning for example 2nd 
-        #box will be 1 * 3 starting from 3rd iteration so 4th positions and ending at 5th iteraton so 6th posistion
-        for j in range(column*3, column*3 + 3): # same thing here but with the columns
+    for i in range(row * 3, row * 3 + 3):
+        for j in range(column * 3, column * 3 + 3):
             if bo[i][j] == num and (i, j) != pos:
                 return False
-    
     return True
+
+
+def solve_with_steps(bo):
+    steps = []
+    _solve_steps(bo, steps)
+    return steps
+
+
+def _solve_steps(bo, steps):
+    find = find_empty(bo)
+    if not find:
+        return True
+    row, col = find
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+            steps.append((row, col, i))
+            if _solve_steps(bo, steps):
+                return True
+            bo[row][col] = 0
+    return False
+
 
 app = Flask(__name__)
 
-grid = [['' for _ in range(9)] for _ in range(9)]
+grid = [[0 for _ in range(9)] for _ in range(9)]
 
 
 @app.route('/')
 def home():
-    values = []
-    grid = [['' for _ in range(9)] for _ in range(9)]
-    return render_template('index.html', grid=grid, values=values)
-
-@app.route('/update', methods=['POST'])
-def update():
-    values = []
-    for i in range(9):
-        for j in range(9):
-            grid[i][j] = request.form.get(f'cell-{i}-{j}', '') #getting values from the grid
+    g = [[0 for _ in range(9)] for _ in range(9)]
+    return render_template('index.html', grid=g)
 
 
+@app.route('/solve', methods=['POST'])
+def solve_route():
+    data = request.get_json()
+    cells = data.get('grid', [])
+    g = [[0] * 9 for _ in range(9)]
+    given = []
+    for item in cells:
+        r, c, v = item['r'], item['c'], item['v']
+        if v != 0:
+            g[r][c] = v
+            given.append([r, c])
 
-    for i in range(9):
-        for j in range(9):
-            if grid[i][j] != '':
-                values.append((i,j))
-                grid[i][j] = int(grid[i][j]) #changing all the '' values to 0
-            else:
-                grid[i][j] = 0
-    solve(grid) #solving the grid
+    board = copy.deepcopy(g)
+    steps = solve_with_steps(board)
 
-    return render_template('update.html', grid=grid, values=values)
+    solved = []
+    for r, c, v in steps:
+        if [r, c] not in given:
+            solved.append({'r': r, 'c': c, 'v': v})
 
-@app.route('/solve_grid', methods =['POST'])
-def solve_grid():
-    return redirect(url_for('home'))
+    return jsonify({'solved': solved, 'success': len(solved) > 0})
+
 
 if __name__ == '__main__':
-    app.run(debug= True)
-
-
-
-
-
-
+    app.run(debug=True)
